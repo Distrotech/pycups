d7cbf7d28c4f3efa248cd5e8b69ce1db7fe1d9b4
Tim Waugh <twaugh@redhat.com>
2011-06-27 11:27:02 +0100

    Link against libiconv on darwin (trac #15).

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from distutils.core import setup, Extension
+import sys
 VERSION="1.9.57"
+libraries=["cups"]
+
+if sys.platform == "darwin":
+	libraries.append ("iconv")
+
 setup (name="pycups",
        version=VERSION,
        ext_modules=[Extension("cups",
                               ["cupsmodule.c", "cupsconnection.c",
                                "cupsppd.c", "cupsipp.c"],
-                              libraries=["cups"],
+                              libraries=libraries,
                               define_macros=[("VERSION", '"%s"' % VERSION)])])
