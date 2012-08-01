52b4eb8c1b54a16dbeab6d9abe4ad46b619bf0fb
Tim Waugh <twaugh@redhat.com>
2012-08-01 12:07:08 +0100

    1.9.62

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ EPSON-Stylus-D78 usb://EPSON/Stylus%20D78
 
 from distutils.core import setup, Extension
 import sys
-VERSION="1.9.61"
+VERSION="1.9.62"
 libraries=["cups"]
 
 if sys.platform == "darwin":
